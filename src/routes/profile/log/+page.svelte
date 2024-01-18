8fff5868124e2c6327c6033e4136a7af4324c3e4
Nikoloz Razmadze <nikoloz@razmadze.me>
2024-01-18 05:43:08 +0400

    Add totals

--- src/routes/profile/log/+page.svelte
+++ src/routes/profile/log/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { invalidateAll } from '$app/navigation';
 	import { getName } from '../../../util/name.js';
+	import Table from './table.svelte';
 
 	export let data;
 
@@ -28,35 +29,6 @@
 	{/if}
 
 	{#each Object.keys(grouped) as user}
-		<div class="overflow-x-scroll mb-4">
-			<table class="table">
-				<thead>
-					<tr>
-						<th colspan="3" class="text-xl capitalize">
-							{user}
-							{#if user === name}
-								(You)
-							{/if}
-						</th>
-					</tr>
-					<tr>
-						<th class="w-2/5">Drink</th>
-						<th class="w-1/5 text-right">Volume</th>
-						<th class="w-1/5 text-right">Alcohol</th>
-						<th class="w-1/5 text-right">Calories</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each grouped[user] as entry}
-						<tr>
-							<td class="w-2/5">{entry.name}</td>
-							<td class="w-1/5 text-right">{entry.volume / 1000} l</td>
-							<td class="w-1/5 text-right">{entry.alcohol / 1000} l</td>
-							<td class="w-1/5 text-right">{Math.round(entry.alcohol * 7)} kcal</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
+		<Table data={grouped[user]} username={user} owner={user === name} />
 	{/each}
 </main>
---
+++ src/routes/profile/log/table.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	export let username: string;
+	export let data: Entry[];
+	export let owner = false;
+
+	const totalAlcohol = data.reduce((a, b) => a + b.alcohol, 0);
+	const ALCOHOL_DENSITY = 0.79;
+</script>
+
+<div class="overflow-x-scroll mb-4">
+	<table class="table">
+		<thead>
+			<tr>
+				<th colspan="3" class="text-xl capitalize" class:text-white={owner}>
+					{username}
+					{#if owner}
+						(You)
+					{/if}
+				</th>
+			</tr>
+			<tr>
+				<th class="w-2/5">Drink</th>
+				<th class="w-1/5 text-right">Volume</th>
+				<th class="w-1/5 text-right">Alcohol</th>
+				<th class="w-1/5 text-right">Calories</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data as entry}
+				<tr>
+					<td class="w-2/5">{entry.name}</td>
+					<td class="w-1/5 text-right">{entry.volume / 1000} l</td>
+					<td class="w-1/5 text-right">{entry.alcohol / 1000} l</td>
+					<td class="w-1/5 text-right">{Math.round(entry.alcohol * ALCOHOL_DENSITY)} kcal</td>
+				</tr>
+			{/each}
+			<tr>
+				<td colspan="2" class="w-3/5 font-bold">Total</td>
+				<td class="w-1/5 text-right font-bold">{totalAlcohol / 1000} l</td>
+				<td class="w-1/5 text-right font-bold">{Math.round(totalAlcohol * ALCOHOL_DENSITY)} kcal</td
+				>
+			</tr>
+		</tbody>
+	</table>
+</div>
