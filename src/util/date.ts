c800333fe0a91faf002fcebf2434f7df799ab959
Nikoloz Razmadze <nikoloz@razmadze.me>
2024-01-19 01:27:31 +0400

    Fix date

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,9 +1,9 @@
 export const getDate = (date?: Date) => {
 	date ??= new Date();
 
-	const year = String(date.getFullYear()).padStart(2, '0');
-	const month = String(date.getMonth() + 1).padStart(2, '0');
-	const day = String(date.getDate()).padStart(2, '0');
+	const year = String(date.getUTCFullYear()).padStart(2, '0');
+	const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+	const day = String(date.getUTCDate()).padStart(2, '0');
 
 	return `${year}-${month}-${day}`;
 };
