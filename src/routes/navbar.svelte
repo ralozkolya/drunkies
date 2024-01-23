1b49c6289a293b13c51ebe3234ee8707f9bb9c17
Nikoloz Razmadze <nikoloz@razmadze.me>
2024-01-24 03:43:41 +0400

    Try to fix dropdown

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -26,7 +26,7 @@
 		</div>
 		{#if user}
 			<div class="dropdown dropdown-end sm:hidden">
-				<button class="btn m-1">
+				<div tabindex="0" role="button" class="btn m-1">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -39,10 +39,10 @@
 							d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
 						></path></svg
 					>
-				</button>
+				</div>
 				<div
 					tabindex="0"
-					class="dropdown-content z-[1] menu p-2 shadow bg-neutral rounded-box w-52 text-lg"
+					class="dropdown-content z-[1] menu p-2 shadow bg-neutral rounded-box w-52"
 				>
 					<span class="capitalize p-4 text-sm">{name}</span>
 					<ul>
