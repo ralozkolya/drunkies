715caea07318197ccfd5a2acc913cf74940e1538
Nikoloz Razmadze <nikoloz@razmadze.me>
2024-02-01 19:04:09 +0400

    Set up local development

---
+++ supabase/seed.sql
@@ -0,0 +1,7 @@
+insert into drinks (name, alcohol, volume, icon)
+values
+('Beer', 5, 400, 'beer-mug-empty'),
+('Wine', 12, 150, 'wine-glass'),
+('Cocktail', 5, 400, 'martini-glass-citrus'),
+('Shot', 40, 50, 'whiskey-glass'),
+('Coca-Cola Zero', 0, 330, 'glass-water');
